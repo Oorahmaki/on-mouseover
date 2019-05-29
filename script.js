7a7099a556757b3f2b357e3d6fb42eb6beb24482
Oohrahmaki <nettie.smith@rocketmail.com>
2019-05-29 23:49:32 +0100

    add functionality - speech

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const eye = document.querySelectorAll(".eye");
 const eyelidL = document.getElementById("eyelidL");
 const eyelidR = document.getElementById("eyelidR");
+const mouth = document.querySelector(".mouth");
+const speech = document.getElementById("talkbubble");
 
 for (let i = 0; i < eye.length; i++){
     eye[i].addEventListener("mouseover", function(){
@@ -12,3 +14,13 @@ for (let i = 0; i < eye.length; i++){
         eyelidR.style.visibility = "hidden";
     })
 }
+
+mouth.addEventListener("mouseover", function(){
+    const name = prompt("Hello, who are you?", "Type Name Here")
+    speech.style.visibility = "visible";
+    speech.innerHTML = "Hello " + name + ", it's nice to meet you!"
+})
+
+mouth.addEventListener("mouseout", function(){
+    speech.style.visibility = "hidden";
+})
